31280153850354e5b39a64a190513a469392acfb
Your Name <you@example.com>
2015-12-28 20:17:49 +0600

    init

---
+++ Facebook API Example Project/src/FacebookApiExample.java
@@ -0,0 +1,25 @@
+import org.json.JSONArray;
+
+import com.google.code.facebookapi.FacebookException;
+import com.google.code.facebookapi.FacebookJsonRestClient;
+
+
+public class FacebookApiExample {
+
+	private static String apikey;
+	private static String secretkey;
+	private static String sessionId;
+	
+	public FacebookApiExample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
